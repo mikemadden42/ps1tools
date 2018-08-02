1ed9f33c69912f75ae37759a9862e1c6d79c9219
Michael Madden <mikemadden42@icloud.com>
2018-08-01 21:22:52 -0500

    First version of SetupApps.ps1

---
+++ SetupApps.ps1
@@ -0,0 +1,34 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install 7zip -y
+choco install ag -y
+choco install cmake -y
+choco install cmder -y
+choco install curl -y
+choco install dependencywalker -y
+choco install etcher -y
+choco install foxitreader -y
+choco install git -y
+choco install golang -y
+choco install googlechrome -y
+choco install hipchat -y
+choco install itunes -y
+choco install jdk8 -y
+choco install mls-software-openssh -y
+choco install ninja -y
+choco install nmap -y
+choco install powershell -y
+choco install putty -y
+choco install python3 -y
+choco install slack -y
+choco install sysinternals -y
+choco install treesizefree -y
+choco install vim -y
+choco install visualstudio2017-workload-vctools -y
+choco install visualstudio2017buildtools -y
+choco install wget -y
+choco install windows-sdk-10.1 -y
+choco install winmerge -y
+choco install wireshark -y
+choco install youtube-dl -y
