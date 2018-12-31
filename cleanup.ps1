cccbc37d9ea905819ab1f43c9543f8b331e8b4e7
Michael Madden <mikemadden42@icloud.com>
2018-12-31 11:18:18 -0600

    Add cleanup.ps1

---
+++ cleanup.ps1
@@ -0,0 +1,4 @@
+﻿Remove-Item -Force -Recurse $HOME\AppData\Local\NuGet\Cache\*
+Remove-Item -Force -Recurse $HOME\AppData\Local\Spotify\Data\*
+Remove-Item -Force -Recurse $HOME\AppData\Roaming\Slack\Cache\*
+#Remove-Item -Force -Recurse $HOME\AppData\Local\Temp\*
