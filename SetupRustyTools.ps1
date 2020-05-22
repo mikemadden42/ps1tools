fe981eb950f62e0266956bccdbeb09ed0e993465
Michael Madden <mikemadden42@icloud.com>
2020-05-22 13:41:15 -0500

    Add rust scripts.

---
+++ SetupRustyTools.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; cargo -v clean; Write-Output "++++"; Set-Location .. }
+
+Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; git clean -fdx; git reset --hard HEAD; Write-Output "++++"; Set-Location .. }
+
+Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; git pull --rebase; Write-Output "++++"; Set-Location .. }
+
+Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; cargo build --release; Write-Output "++++"; Set-Location .. }
+		
+Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; Copy-Item target\release\*.exe C:\Users\mjm\Desktop\rusty-tools; Write-Output "++++"; Set-Location .. }
---
+++ UpdateRust.ps1
@@ -0,0 +1,6 @@
+rustup self update
+rustup update
+
+rustup component add clippy
+rustup component add rls rust-analysis
+rustup component add rustfmt
