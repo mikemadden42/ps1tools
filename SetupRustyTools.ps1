4878c31dc10432f7466af9d61962f2b52935d291
Michael Madden <mikemadden42@icloud.com>
2020-05-22 13:46:41 -0500

    Clean up whitespace.

--- SetupRustyTools.ps1
+++ SetupRustyTools.ps1
@@ -5,5 +5,5 @@ Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.N
 Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; git pull --rebase; Write-Output "++++"; Set-Location .. }
 
 Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; cargo build --release; Write-Output "++++"; Set-Location .. }
-		
+
 Get-ChildItem -path . | ForEach-Object { Write-Output $_.Name; Set-Location $_.Name; Copy-Item target\release\*.exe C:\Users\mjm\Desktop\rusty-tools; Write-Output "++++"; Set-Location .. }
