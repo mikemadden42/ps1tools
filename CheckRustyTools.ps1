fa7b93061695b7f72ca24de0c0e0030a7da39437
Michael Madden <mikemadden42@icloud.com>
2020-06-07 01:14:09 +0000

    Add Windows Server 2019 support.

--- CheckRustyTools.ps1
+++ CheckRustyTools.ps1
@@ -1,1 +1,2 @@
-Get-ChildItem -path . | ForEach-Object { $exists = (Test-Path C:\Users\mjm\Desktop\rusty-tools\$_.exe); if (-not $exists) { Write-Output $_.Name }; }
+$user = $env:USERNAME
+Get-ChildItem -path . | ForEach-Object { $exists = (Test-Path C:\Users\$user\Desktop\rusty-tools\$_.exe); if (-not $exists) { Write-Output $_.Name }; }
