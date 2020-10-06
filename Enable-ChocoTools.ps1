21d73660e7289dd6fc89d63b81f0435db00b246a
Michael Madden <mikemadden42@icloud.com>
2020-10-06 18:56:49 -0500

    Add bat, bottom, ripgrep, sd-cli.

--- Enable-ChocoTools.ps1
+++ Enable-ChocoTools.ps1
@@ -6,10 +6,13 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 # Install packages one at a time.
 choco install 7zip -y
 choco install ag -y
+choco install bat -y
 choco install brave -y
+choco install bottom -y
 choco install cmake -y
 choco install curl -y
 choco install dependencywalker -y
+choco install fd -y
 choco install git -y
 choco install ghidra -y
 choco install golang -y
@@ -21,6 +24,8 @@ choco install nmap -y
 choco install notepadplusplus -y
 choco install putty -y
 choco install python -y
+choco install ripgrep -y
+choco install sd-cli -y
 choco install slack -y
 choco install sysinternals -y
 choco install visualstudio2019-workload-vctools -y
@@ -32,4 +37,4 @@ choco install winmerge -y
 choco install wireshark -y
 
 # Install packages all at once.
-choco install 7zip ag brave cmake curl dependencywalker ghidra git golang googlechrome keepassxc microsoft-windows-terminal ninja nmap notepadplusplus putty python slack sysinternals visualstudio2019-workload-vctools visualstudio2019buildtools vscode wget windows-sdk-10.1 winmerge wireshark -y
+choco install 7zip ag bat brave bottom cmake curl dependencywalker fd git ghidra golang googlechrome keepassxc microsoft-windows-terminal ninja nmap notepadplusplus putty python ripgrep sd-cli slack sysinternals visualstudio2019-workload-vctools visualstudio2019buildtools vscode wget windows-sdk-10.1 winmerge wireshark 
