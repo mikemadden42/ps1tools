366917f444f87fbc895eecfeefb8b0551ef71aef
Michael Madden <mikemadden42@icloud.com>
2022-06-13 19:03:45 -0500

    Remove Get-InstalledSoftware.ps1.

--- Get-InstalledSoftware.ps1
+++
@@ -1,6 +0,0 @@
-$computer = "."
-$products = Get-CimInstance -Class "Win32_Product" -Namespace "root\CIMV2" -ComputerName $computer
-
-foreach ($package in $products) {
-    Write-Output $package.Caption
-}
